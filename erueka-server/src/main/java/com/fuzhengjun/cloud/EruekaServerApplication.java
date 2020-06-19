4143642bebb43c91e463fc08c63871a8e560767f
LuoJhno <540088249@qq.com>
2020-06-19 16:46:59 +0800

    feature ## springCloud erueka-server feature ## springCloud erueka-client feature ## springCloud erueka-security-server feature ## springCloud erueka-server2 feature ## springCloud hystrix-dashboard feature ## springCloud hystrix-service feature ## springCloud ribbon-service feature ## springCloud user-service

---
+++ erueka-server/src/main/java/com/fuzhengjun/cloud/EruekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.fuzhengjun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EruekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruekaServerApplication.class, args);
+    }
+
+}
---
+++ user-service/src/main/java/com/fuzhengjun/business/service/UserService.java
@@ -0,0 +1,19 @@
+package com.fuzhengjun.business.service;
+
+import com.fuzhengjun.business.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+    User getByUsername(String username);
+
+    List<User> getUserByIds(List<Long> ids);
+}
